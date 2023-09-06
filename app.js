c2736d26868b5d294f496e58c381acab315dfe04
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2023-09-06 20:10:13 +0800

    rendering user authentication

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
+const connectDB = require('./server/config/db')
+const session = require('express-session')
+const passport = require('passport')
+const MongoStore = require('connect-mongo')
 
 const app = express()
 const port = 5000 || process.env.PORT
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MongoDB_URI
+    })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+//DB connection
+connectDB()
 
 //for Static files
 app.use(express.static('public'))
@@ -18,6 +36,7 @@ app.set('layout','./layouts/main')
 app.set('view engine','ejs')
 
 //Routes
+app.use('/', require('./server/routes/auth'))
 app.use('/', require('./server/routes/index'))
 app.use('/', require('./server/routes/dashboard'))
 
